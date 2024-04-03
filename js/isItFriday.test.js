2459fdba5f5b8a51f1429797d41ace221d978beb
Jonathan Lison <jlison@bidmc.harvard.edu>
2024-04-03 10:57:55 -0400

    Add a test

--- js/isItFriday.test.js
+++ js/isItFriday.test.js
@@ -3,3 +3,7 @@ const isItFriday = require('./isItFriday')
 test('Invokes isItFriday function', () => {
     expect(isItFriday(new Date())).toBeDefined();
 })
+
+test('Today is not Friday', () => {
+    expect(isItFriday(new Date())) === false;
+})
